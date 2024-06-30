0f525478de9bf50985a0eaf999dfca917343f4e5
Shahid Manzoor Rather <135219736+ShahidManzoor5@users.noreply.github.com>
2024-06-30 04:54:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,60 @@
-//web server for comments
+// create web server for comments
+// 1. create a web server
+// 2. create a route for comments
+// 3. create a route for comments/new
+// 4. create a route for comments/:id
+// 5. create a route for comments/:id/edit
+// 6. create a route for comments/:id/delete
+// 7. create a route for comments/:id/like
+// 8. create a route for comments/:id/dislike
+// 9. create a route for comments/:id/report
+
+// 1. create a web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 2. create a route for comments
+app.get('/comments', (req, res) => {
+  res.send('Comments');
+});
+
+// 3. create a route for comments/new
+app.get('/comments/new', (req, res) => {
+  res.send('New Comment');
+});
+
+// 4. create a route for comments/:id
+app.get('/comments/:id', (req, res) => {
+  res.send('Comment ID: ' + req.params.id);
+});
+
+// 5. create a route for comments/:id/edit
+app.get('/comments/:id/edit', (req, res) => {
+  res.send('Edit Comment ID: ' + req.params.id);
+});
+
+// 6. create a route for comments/:id/delete
+app.get('/comments/:id/delete', (req, res) => {
+  res.send('Delete Comment ID: ' + req.params.id);
+});
+
+// 7. create a route for comments/:id/like
+app.get('/comments/:id/like', (req, res) => {
+  res.send('Like Comment ID: ' + req.params.id);
+});
+
+// 8. create a route for comments/:id/dislike
+app.get('/comments/:id/dislike', (req, res) => {
+  res.send('Dislike Comment ID: ' + req.params.id);
+});
+
+// 9. create a route for comments/:id/report
+app.get('/comments/:id/report', (req, res) => {
+  res.send('Report Comment ID: ' + req.params.id);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
